5a41e9643a981aed5d3d048af99bd7a3ac73b179
omidoyin--Git <omidoyinayodeji@yahoo.com>
2023-08-02 16:11:02 +0100

    update on routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 // import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import HomePage from './componets/HomePage';
-import Projects from './componets/Projects';
-import Navigation from './componets/Navigation';
-import Footer from './componets/Footer';
-import ProjectDetails from './componets/ProjectDetails';
-import Experience from './componets/Experience';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./componets/HomePage";
+import Projects from "./componets/Projects";
+import Navigation from "./componets/Navigation";
+import Footer from "./componets/Footer";
+import ProjectDetails from "./componets/ProjectDetails";
+import Experience from "./componets/Experience";
+import ScrollToTop from "./componets/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navigation/>
-        <Routes>
-          <Route path='/'   element ={<HomePage/>} /> 
-          <Route path='/projects'   element ={<Projects/>} /> 
-          <Route path='/projects/:id'   element ={<ProjectDetails/>} /> 
-          <Route path='/experience'   element ={<Experience/>} /> 
-        
-        </Routes>
-        <Footer/>
+        <ScrollToTop>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:id" element={<ProjectDetails />} />
+            <Route path="/experience" element={<Experience />} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
       </Router>
-
- 
     </div>
   );
 }
